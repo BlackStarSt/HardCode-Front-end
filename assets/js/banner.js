deb3c18ac83703df4f8bbfdaad38972f217e421a
BlackStarSt <sthefanya86@gmail.com>
2022-09-10 14:18:20 -0300

    Modificando banner

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -7,5 +7,15 @@ $('index.html').ready(function() {
         appendDots: $('.barra__progresso'),
         dots: true,
         arrows: false,
+        asNavFor: '.subtitulo__container, .banner__descricao'
+    });
+    $('.subtitulo__container, .banner__descricao').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        focusOnSelect: true,
     });
 });
